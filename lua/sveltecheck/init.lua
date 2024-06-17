4a69eeb97172e416b19dd614dc6a1ee9b3cdc29d
Stephen <stephengunn@gmail.com>
2024-06-17 12:30:48 -0500

    busy

--- lua/sveltecheck/init.lua
+++ lua/sveltecheck/init.lua
@@ -8,8 +8,6 @@ local default_config = {
 local config = vim.deepcopy(default_config)
 local spinner_index = 1
 local spinner_timer = nil
-local error_count = 0
-local warning_count = 0
 local start_time = 0
 local end_time = 0
 local total_time = 0
@@ -24,13 +22,11 @@ local function start_spinner()
     spinner_timer = vim.loop.new_timer()
 
     spinner_timer:start(
-        0, -- initial delay in milliseconds (must be integer)
-        100, -- repeat interval in milliseconds (must be integer)
+        0,
+        100,
         vim.schedule_wrap(function()
-            -- Update the notification message with spinner frames
             vim.notify("Running Svelte Check... " .. config.spinner_frames[spinner_index], "info", {
-                id = notification_id, -- Update existing notification
-                timeout = 0, -- Display indefinitely until cleared manually
+                id = notification_id,
                 log = false,
             })
 
@@ -84,7 +80,7 @@ M.run = function()
             end
 
             -- calculate the total time
-            total_time = end_time - start_time
+            -- total_time = end_time - start_time
 
             if #quickfix_list > 0 then
                 vim.fn.setqflist({}, "r", { title = config.command .. " output", items = quickfix_list })
