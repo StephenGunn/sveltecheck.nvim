97a7eb1627a0a16f38011a29c041ddf0e851fe40
Stephen <stephengunn@gmail.com>
2024-06-17 12:56:27 -0500

    no space

--- lua/sveltecheck/init.lua
+++ lua/sveltecheck/init.lua
@@ -106,7 +106,7 @@ M.run = function()
         end
     end
 
-    local job_id = vim.fn.jobstart(config.command .. '--output "machine"', {
+    local job_id = vim.fn.jobstart(config.command .. ' --output "machine"', {
         stdout_buffered = true,
         stderr_buffered = true,
         on_stdout = function(_, data)
