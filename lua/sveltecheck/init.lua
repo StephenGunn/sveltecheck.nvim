a4ad11bdb11925f5fff65700dff8cc0fb529891f
Stephen <stephengunn@gmail.com>
2024-06-17 17:17:36 -0500

    adding the option for telescope

--- lua/sveltecheck/init.lua
+++ lua/sveltecheck/init.lua
@@ -2,6 +2,7 @@ local M = {}
 
 local default_config = {
     command = "pnpm run check",
+    use_telescope = true, -- New configuration option
     spinner_frames = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" },
     debug_mode = false,
 }
@@ -104,7 +105,63 @@ local function on_output(_, data, event)
 
     local quickfix_list = make_quickfix_list(lines)
 
-    if event == "stdout" then
+    if config.use_telescope then
+        if not pcall(require, "telescope") then
+            error("Telescope.nvim not found. Please install it before using this feature.")
+        end
+
+        local telescope = require("telescope")
+        local pickers = require("telescope.pickers")
+        local finders = require("telescope.finders")
+        local actions = require("telescope.actions")
+        local previewers = require("telescope.previewers")
+        local entry_display = require("telescope.pickers.entry_display")
+
+        local results = {}
+        for _, item in ipairs(quickfix_list) do
+            table.insert(results, {
+                filename = item.filename,
+                lnum = item.lnum,
+                col = item.col,
+                text = item.text,
+                display = entry_display.create({
+                    separator = " ▏",
+                    items = {
+                        { width = 50 },
+                        { width = 10 },
+                        { remaining = true },
+                    },
+                }),
+            })
+        end
+
+        pickers
+            .new({}, {
+                prompt_title = "Svelte Check Results",
+                finder = finders.new_table({
+                    results = results,
+                    entry_maker = function(entry)
+                        return {
+                            valid = true,
+                            value = entry,
+                            ordinal = entry.filename .. ":" .. entry.lnum .. ":" .. entry.col .. " " .. entry.text,
+                            display = entry.display,
+                        }
+                    end,
+                }),
+                sorter = require("telescope.config").values.generic_sorter({}),
+                attach_mappings = function(prompt_bufnr, map)
+                    actions.select_default:replace(function()
+                        local selection = actions.get_selected_entry()
+                        actions.close(prompt_bufnr)
+                        vim.cmd(string.format("%d%s", selection.value.lnum, "G"))
+                        vim.cmd("norm zz")
+                    end)
+                    return true
+                end,
+            })
+            :find()
+    else
         if #quickfix_list > 0 then
             vim.fn.setqflist({}, "r", { title = "Svelte Check", items = quickfix_list })
             vim.cmd("copen")
@@ -137,87 +194,20 @@ M.run = function()
     vim.fn.jobwait({ job_id }, 1000)
 end
 
-function M.setup()
-    vim.api.nvim_create_command("SvelteCheck", function()
+function M.setup(user_config)
+    if user_config then
+        config = vim.tbl_deep_extend("force", config, user_config)
+    end
+
+    vim.api.nvim_create_user_command("SvelteCheck", function()
         M.run()
-    end, { nargs = 0, desc = "Run `svelte-check` asynchronously and load the results into a qflist" })
+    end, { desc = "Run `svelte-check` asynchronously and load the results into a qflist", force = true })
 
-    vim.api.nvim_create_command("SvelteCheckTelescope", function()
-        if not pcall(require, "telescope") then
-            error("Telescope.nvim not found. Please install it before using this feature.")
-        end
-
-        local telescope = require("telescope")
-        local pickers = require("telescope.pickers")
-        local finders = require("telescope.finders")
-        local actions = require("telescope.actions")
-        local previewers = require("telescope.previewers")
-        local entry_display = require("telescope.pickers.entry_display")
-
-        local function handle_output(data)
-            local svelte_check_output = table.concat(data, "\n")
-            local lines = vim.split(svelte_check_output, "\n")
-
-            local quickfix_list = make_quickfix_list(lines)
-
-            if #quickfix_list > 0 then
-                local results = {}
-                for _, item in ipairs(quickfix_list) do
-                    table.insert(results, {
-                        filename = item.filename,
-                        lnum = item.lnum,
-                        col = item.col,
-                        text = item.text,
-                        display = entry_display.create({
-                            separator = " ▏",
-                            items = {
-                                { width = 50 },
-                                { width = 10 },
-                                { remaining = true },
-                            },
-                        }),
-                    })
-                end
-
-                pickers
-                    .new({}, {
-                        prompt_title = "Svelte Check Results",
-                        finder = finders.new_table({
-                            results = results,
-                            entry_maker = function(entry)
-                                return {
-                                    valid = true,
-                                    value = entry,
-                                    ordinal = entry.filename
-                                        .. ":"
-                                        .. entry.lnum
-                                        .. ":"
-                                        .. entry.col
-                                        .. " "
-                                        .. entry.text,
-                                    display = entry.display,
-                                }
-                            end,
-                        }),
-                        sorter = require("telescope.config").values.generic_sorter({}),
-                        attach_mappings = function(prompt_bufnr, map)
-                            actions.select_default:replace(function()
-                                local selection = actions.get_selected_entry()
-                                actions.close(prompt_bufnr)
-                                vim.cmd(string.format("%d%s", selection.value.lnum, "G"))
-                                vim.cmd("norm zz")
-                            end)
-                            return true
-                        end,
-                    })
-                    :find()
-            else
-                print("No results to show.")
-            end
-        end
-
-        M.run()
-    end, { nargs = 0, desc = "Run `svelte-check` asynchronously and load the results into Telescope" })
+    if config.use_telescope then
+        vim.api.nvim_create_command("SvelteCheckTelescope", function()
+            M.run()
+        end, { nargs = 0, desc = "Run `svelte-check` asynchronously and load the results into Telescope" })
+    end
 end
 
 return M
